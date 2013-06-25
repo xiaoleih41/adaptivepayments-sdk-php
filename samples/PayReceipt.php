1c7fc8de29442556c4d546ab4c42a0b5a7147cd3
Ganesh Hegde <1ganesh1@facebook.com>
2013-06-25 23:08:19 +0530

    sample update for using dynamic config

--- samples/PayReceipt.php
+++ samples/PayReceipt.php
@@ -190,9 +190,9 @@ if($_POST['emailIdentifier'] != "" || $_POST['senderCountryCode'] != "" || $_POS
 /*
  * 	 ## Creating service wrapper object
 Creating service wrapper object to make API call and loading
-configuration file for your credentials and endpoint
-*/
-$service = new AdaptivePaymentsService();
+Configuration::getSignatureConfig() returns array that contains credential and config parameters
+ */
+$service = new AdaptivePaymentsService(Configuration::getSignatureConfig());
 try {
 	/* wrap API method calls on the service object with a try catch */
 	$response = $service->Pay($payRequest);
--- samples/PreapprovalDetails.php
+++ samples/PreapprovalDetails.php
@@ -1,8 +1,6 @@
 <?php
 require_once('PPBootStrap.php');
 
-$logger = new PPLoggingManager('PreapprovalDetails');
-
 ?>
 <html>
 <head>
