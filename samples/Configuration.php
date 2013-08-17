a6cc612b4fb02b37eb0e66cdcd75ee9f4248df35
lathavairamani <lvairamani@paypal.com>
2013-08-17 11:21:23 +0530

    updated config method names in samples and IPN-README.md

--- samples/Configuration.php
+++ samples/Configuration.php
@@ -1,43 +1,42 @@
-<?php Class Configuration
+<?php 
+class Configuration
 {
-	public static function getSignatureConfig()
+	public static function getConfig()
 	{
 		$config = array(
+				// values: 'sandbox' for testing
+				//		   'live' for production
+				"mode" => "sandbox"
+	
+				// These values are defaulted in SDK. If you want to override default values, uncomment it and add your value.
+				// "http.ConnectionTimeOut" => "5000",
+				// "http.Retry" => "2",
+				// "http.ReadTimeOut" => "30000",
+				// "http.MaxConnection" => "100"
+		);
+		return $config;
+	}
+	
+	// Creates a configuration array containing credentials and other required configuration parameters.
+	public static function getAcctAndConfig()
+	{
+		$config = array(
+				// Signature Credential
 				"acct1.UserName" => "jb-us-seller_api1.paypal.com",
 				"acct1.Password" => "WX4WTU3S8MY44S7F",
 				"acct1.Signature" => "AFcWxV21C7fd0v3bYYYRCpSSRl31A7yDhhsPUU2XhtMoZXsWHFxu-RWy",
-				"acct1.AppId" => "APP-80W284485P519543T",
+				"acct1.AppId" => "APP-80W284485P519543T"
 				
-				// values: 'sandbox' for testing
-				//		   'live' for production
-				"mode" => "sandbox",
-			
-				//Connection Information
-				"http.ConnectionTimeOut" => "30",
-				"http.Retry" => "2",
-				//http.Proxy=http://[username:password]@hostname[:port][/path]
-				);
-		return $config;
-	}
-	public static function getCertificateConfig()
-	{
-		$config = array(
-				"acct1.UserName" => "certuser_biz_api1.paypal.com",
-				"acct1.Password" => "D6JNKKULHN3G5B8A",
+				// Sample Certificate Credential
+				// "acct1.UserName" => "certuser_biz_api1.paypal.com",
+				// "acct1.Password" => "D6JNKKULHN3G5B8A",
 				// Certificate path relative to config folder or absolute path in file system
-				"acct1.CertPath" => "cert_key.pem",
-				"acct1.AppId" => "APP-80W284485P519543T",
+				// "acct1.CertPath" => "cert_key.pem",
+				// "acct1.AppId" => "APP-80W284485P519543T"
+				);
 		
-				// values: 'sandbox' for testing
-				//		   'live' for production
-				"mode" => "sandbox",
-		
-				//Connection Information
-				"http.ConnectionTimeOut" => "30",
-				"http.Retry" => "2",
-				//http.Proxy=http://[username:password]@hostname[:port][/path]
-		);	
-		return $config;
+		return array_merge($config, self::getConfig());;
 	}
+
 }
 
